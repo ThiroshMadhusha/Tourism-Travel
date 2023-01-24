cbca941ebc2a369cafffd9d22fa0bc2011c18903
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-01-25 01:17:30 +0530

    complete offer section

---
+++ src/Components/offers/offersdata.js
@@ -0,0 +1,36 @@
+import img1 from "../../assets/images/img1.jpg";
+import img2 from "../../assets/images/img1.jpg";
+import img3 from "../../assets/images/img1.jpg";
+
+export const Data = [
+  {
+    id: 1,
+    image: img1,
+    title: "Hello World",
+    location: "Tangalle",
+    price: "$40",
+    offer: "30%",
+    bed: 2,
+    bathroom: 1,
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "Hello World",
+    location: "Tangalle",
+    price: "$40",
+    offer: "35%",
+    bed: 1,
+    bathroom: 1,
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "Hello World",
+    location: "Tangalle",
+    price: "$40",
+    offer: "20%",
+    bed: 3,
+    bathroom: 1,
+  },
+];
